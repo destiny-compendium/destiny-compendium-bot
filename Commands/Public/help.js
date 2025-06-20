9ea8a2b4e85be043e1c074ae21ab296aa07d24d2
DcruBro <jonaskn914@gmail.com>
2025-06-20 17:52:49 +0200

    Test embeds for everything

---
+++ Commands/Public/help.js
@@ -0,0 +1,80 @@
+const { SlashCommandBuilder, CommandInteraction, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+
+function failEmbed() {
+  return new EmbedBuilder()
+	  .setColor(0xFF0000)
+	  .setTitle("Query Failed")
+	  .setAuthor({ name: "Destiny Compendium" })
+    .setDescription("Sorry, but your query matched no results.")
+	  .setThumbnail("https://i.imgur.com/MNab4aw.png")
+	  .setTimestamp();
+}
+
+function errorEmbed() {
+  return new EmbedBuilder()
+	  .setColor(0xFF0000)
+	  .setTitle("An Error Occurred")
+	  .setAuthor({ name: "Destiny Compendium" })
+    .setDescription("Sorry, but an internal error occurred during your query.")
+	  .setThumbnail("https://i.imgur.com/MNab4aw.png")
+	  .setTimestamp();
+}
+
+function timeoutEmbed() {
+  return new EmbedBuilder()
+	  .setColor(0xFF0000)
+	  .setTitle("Timed Out")
+	  .setAuthor({ name: "Destiny Compendium" })
+    .setDescription("Sorry, but your query timed out during processing.")
+	  .setThumbnail("https://i.imgur.com/MNab4aw.png")
+	  .setTimestamp();
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("help")
+        .setDescription("Bot help menu")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Everyone),
+
+        async execute(interaction, client) {
+            await interaction.deferReply();
+
+            let replied = false;
+
+            // Set your timeout (e.g., 60 seconds)
+            const timeout = setTimeout(async () => {
+              if (!replied) {
+                await interaction.editReply({ embeds: [timeoutEmbed()] });
+                replied = true;
+              }
+            }, 10000); // 60,000 ms = 60 seconds
+
+            try {
+                const embed = new EmbedBuilder()
+                    .setColor(0x0000FF)
+                    .setTitle("Help Menu")
+                    .setAuthor({ name: "Destiny Compendium" })
+                    .setDescription(
+                        `**/query <category> <query>** - Queries the Destiny Compendium spreadsheet for your desired **query** in the selected **category**.`
+                    )
+                    .setThumbnail("https://i.imgur.com/iR1JvU5.png")
+                    .setTimestamp();
+                
+                interaction.editReply({
+                  embeds: [embed],
+                  ephemeral: false
+                });
+              clearTimeout();
+              replied = true;
+            } catch (error) {
+              if (!replied) {
+                await interaction.editReply({ embeds: [errorEmbed()] });
+                replied = true;
+              }
+              clearTimeout();
+              console.error(error);
+            }
+
+            return;
+        },
+};
