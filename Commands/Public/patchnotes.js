465bdaeffcbe79caa986369880d908a181defa86
DcruBro <jonaskn914@gmail.com>
2025-06-20 23:20:16 +0200

    Patch notes command

---
+++ Commands/Public/patchnotes.js
@@ -0,0 +1,85 @@
+const { SlashCommandBuilder, CommandInteraction, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+
+function failEmbed() {
+  return new EmbedBuilder()
+	  .setColor(0xFF0000)
+	  .setTitle("Query Failed")
+	  .setAuthor({ name: "Destiny Compendium" })
+    .setDescription("Sorry, but your query matched no results.")
+	  .setThumbnail("https://i.imgur.com/MNab4aw.png")
+	  .setTimestamp();
+}
+
+function errorEmbed() {
+  return new EmbedBuilder()
+	  .setColor(0xFF0000)
+	  .setTitle("An Error Occurred")
+	  .setAuthor({ name: "Destiny Compendium" })
+    .setDescription("Sorry, but an internal error occurred during your query.")
+	  .setThumbnail("https://i.imgur.com/MNab4aw.png")
+	  .setTimestamp();
+}
+
+function timeoutEmbed() {
+  return new EmbedBuilder()
+	  .setColor(0xFF0000)
+	  .setTitle("Timed Out")
+	  .setAuthor({ name: "Destiny Compendium" })
+    .setDescription("Sorry, but your query timed out during processing.")
+	  .setThumbnail("https://i.imgur.com/MNab4aw.png")
+	  .setTimestamp();
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("patchnotes")
+        .setDescription("Display patch notes")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Everyone),
+
+        async execute(interaction, client) {
+            await interaction.deferReply();
+
+            let replied = false;
+
+            // Set your timeout (e.g., 60 seconds)
+            const timeout = setTimeout(async () => {
+              if (!replied) {
+                await interaction.editReply({ embeds: [timeoutEmbed()] });
+                replied = true;
+              }
+            }, 10000); // 60,000 ms = 60 seconds
+
+            try {
+                const embed = new EmbedBuilder()
+                    .setColor(0x0000FF)
+                    .setTitle("Patch Notes")
+                    .setAuthor({ name: "Destiny Compendium" })
+                    .setDescription(
+                        `**Version 1.0.0**\n
+                        This version is the base version, and implements the Bot's basic features:
+                        - **Queries**
+                        - **Help guide**
+                        - **About section**
+                        - **This menu**`
+                    )
+                    .setThumbnail("https://i.imgur.com/F9KcQzL.png")
+                    .setTimestamp();
+                
+                interaction.editReply({
+                  embeds: [embed],
+                  ephemeral: false
+                });
+              clearTimeout();
+              replied = true;
+            } catch (error) {
+              if (!replied) {
+                await interaction.editReply({ embeds: [errorEmbed()] });
+                replied = true;
+              }
+              clearTimeout();
+              console.error(error);
+            }
+
+            return;
+        },
+};
