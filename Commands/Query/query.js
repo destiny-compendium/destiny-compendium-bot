2af5152415b98ad8c1a435b49f4cf432f336182c
Shiso <66481510+zShiso@users.noreply.github.com>
2025-06-21 21:12:05 +0200

    revert

--- Commands/Query/query.js
+++ Commands/Query/query.js
@@ -234,7 +234,7 @@ module.exports = {
 
               // Rank all matching rows by score (shortest matched cell)
               const maxLookahead = category === "Exotic Weapons" ? 3 : 2;
-              const isArtifact = (category === "Artifact Perks" || category === "Old Episodic Artifact Perks");
+              const isArtifact = (category === "Artifact Perks" || category === "Old Episodic Artifact Perks")=;
               let match = [];
 
               try {
@@ -276,7 +276,7 @@ module.exports = {
 
               } catch (error) {
                 if (!replied) {
-                  await interaction.editReply(error);
+                  await interaction.editReply({ embeds: [errorEmbed()] });
                   replied = true;
                 }
                 clearTimeout();
