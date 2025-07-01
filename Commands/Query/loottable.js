51646a710c11fe58b1756541a268918378386bd9
DcruBro <jonaskn914@gmail.com>
2025-07-01 18:07:58 +0200

    Added all loot table descriptions (My hands hurt)

--- Commands/Query/loottable.js
+++ Commands/Query/loottable.js
@@ -47,123 +47,184 @@ const reverseImage = {
     d_sundered: "https://i.imgur.com/hC4skNY.png"
 }
 
+// My hands hurty :(
 const reverseDesc = {
     lw: {
         name: "Last Wish",
         entries: [
             { encounter: "All", desc: "This Raid has no specific loot table." }
-        ]
+        ],
+        notes: "-"
     },
     gos:  {
         name: "Garden of Salvation",
         entries: [
-            { encounter: "1st", desc: `${icons.void}Zealot's Reward, ${icons.kinetic}Accrued Redemption, Leg Armor` },
-            { encounter: "2nd", desc: `${icons.arc}Prophet of Doom, ${icons.void}Reckless Oracle, Arms Armor` },
-            { encounter: "3rd", desc: `${icons.void}Ancient Gospel, ${icons.kinetic}Sacred Provenance, Chest Armor` },
-            { encounter: "4th", desc: `${icons.solar}Omniscient Eye, ${icons.arc}${icons.overload}Divinity, Head Armor, Class Armor` },
-        ]
+            { encounter: "1st - 'Evade the Consecrated Mind'", desc: `${icons.void} Zealot's Reward, ${icons.kinetic} Accrued Redemption, Leg Armor` },
+            { encounter: "2nd - 'Summon the Consecrated Mind'", desc: `${icons.arc} Prophet of Doom, ${icons.void} Reckless Oracle, Arms Armor` },
+            { encounter: "3rd - 'Defeat the Consecrated Mind'", desc: `${icons.void} Ancient Gospel, ${icons.kinetic} Sacred Provenance, Chest Armor` },
+            { encounter: "4th - 'Defeat the Sanctified Mind'", desc: `${icons.solar} Omniscient Eye, ${icons.arc} ${icons.overload} ${icons.exoticengram} Divinity, Head Armor, Class Armor` },
+        ],
+        notes: "-"
     },
     dsc:  {
         name: "Deep Stone Crypt",
         entries: [
-
-        ]
+            { encounter: "1st - 'Crypt Security'", desc: `${icons.solar} Trustee, Arms Armor, Leg Armor, Class Armor` },
+            { encounter: "2nd - 'Atraks-1, Fallen Exo'", desc: `${icons.kinetic} Heritage, ${icons.kinetic} Succession, Arms Armor, Leg Armor, Class Armor` },
+            { encounter: "3rd - 'Descent'", desc: `${icons.kinetic} Heritage, ${icons.arc} Posterity, Arms Armor, Chest Armor, Class Armor` },
+            { encounter: "4th - 'Taniks, the Abomination'", desc: `${icons.void} Commemoration, ${icons.arc} Bequest, ${icons.solar} ${icons.exoticengram} Eyes of Tomorrow, Head Armor, Chest Armor, Leg Armor` },
+        ],
+        notes: "-"
     },
     vog:  {
         name: "Vault of Glass",
         entries: [
-
-        ]
+            { encounter: "1st - 'Confluxes'", desc: `${icons.arc} Found Verdict, ${icons.solar} Vision of Confluence, ${icons.void}, Corrective Meaure, Arms Armor, Class Armor` },
+            { encounter: "2nd - 'Oracles'", desc: `${icons.arc} Found Verdict, ${icons.solar} Vision of Confluence, ${icons.kinetic} Praedyth's Revenge, Arms Armor, Leg Armor` },
+            { encounter: "3rd - 'Templar'", desc: `${icons.kinetic} Fatebringer, ${icons.solar} Vision of Confluence, ${icons.void} Corrective Measure, Arms Armor, Chest Armor` },
+            { encounter: "4th - 'Gatekeepers'", desc: `${icons.arc} Found Verdict, ${icons.kinetic} Fatebringer, ${icons.solar} Hezen Vengeance, Head Armor, Leg Armor` },
+            { encounter: "5th - 'Atheon, Time's Conflux'", desc: `${icons.kinetic} Praedyth's Revenge, ${icons.void} Corrective Measure, ${icons.solar} Hezen Vengeance, ${icons.solar} ${icons.exoticengram} Vex Mythoclast, Head Armor, Chest Armor` },
+        ],
+        notes: "-"
     },
     vow:  {
         name: "Vow of the Disciple",
         entries: [
-
-        ]
+            { encounter: "1st - 'Acquisition'", desc: `${icons.kinetic} Submission, ${icons.stasis} Deliverance, ${icons.solar} Cataclysmic, Head Armor, Chest Armor, Leg Armor` },
+            { encounter: "2nd - 'The Caretaker'", desc: `${icons.kinetic} Submission, ${icons.arc} Insidious, ${icons.solar} Cataclysmic, ${icons.arc} Forbearance, Head Armor, Arms Armor, Class Armor` },
+            { encounter: "3rd - 'Exhibition'", desc: `${icons.kinetic} Submission, ${icons.stasis} Deliverance, Head Armor, Chest Armor, Leg Armor` },
+            { encounter: "4th - 'Rhulk, Disciple of the Witness'", desc: `${icons.arc} Insidious, ${icons.arc} Forbearance, ${icons.solar} Lubrae's Ruin, ${icons.void} ${icons.exoticengram} Collective Obligation, Head Armor, Arms Armor, Class Armor` },
+        ],
+        notes: "-"
     },
     kf:  {
         name: "King's Fall",
         entries: [
-
-        ]
+            { encounter: "1st - 'Gate'", desc: `${icons.void} Doom of Chelchis, ${icons.stasis} Qullim's Terminus, Class Armor` },
+            { encounter: "2nd - 'Basilica'", desc: `${icons.void} Doom of Chelchis, ${icons.stasis} Qullim's Terminus, Chest Armor, Leg Armor` },
+            { encounter: "3rd - 'Warpriest'", desc: `${icons.kinetic} Smite of Merain, ${icons.kinetic} Defiance of Yasmin, ${icons.stasis} Qullim's Terminus, Arms Armor, Chest Armor` },
+            { encounter: "4th - 'Golgoroth'", desc: `${icons.void} Doom of Chelchis, ${icons.solar} Zaouli's Bane, ${icons.arc} Midha's Reckoning, ${icons.stasis} Qullim's Terminus, Head Armor, Leg Armor` },
+            { encounter: "5th - 'The Daughters'", desc: `${icons.void} Doom of Chelchis, ${icons.kinetic} Smite of Merain, ${icons.solar} Zaouli's Bane, ${icons.kinetic} Defiance of Yasmin, Arms Armor, Chest Armor` },
+            { encounter: "6th - 'Oryx, the Taken King'", desc: `Any Weapon, ${icons.kinetic} ${icons.exoticengram} Touch of Malice, Any Armor` },
+        ],
+        notes: "-"
     },
     ron:  {
         name: "Root of Nightmares",
         entries: [
-
-        ]
+            { encounter: "1st - 'Cataclysm'", desc: `${icons.void} Nessa's Oblation, ${icons.solar} Briar's Contempt, ${icons.strand} Koraxis's Distress, Head Armor, Arms Armor, Chest Armor` },
+            { encounter: "2nd - 'Scission'", desc: `${icons.void} Nessa's Oblation, ${icons.solar} Acasia's Dejection, ${icons.strand} Mykel's Reverence, ${icons.strand} Koraxis's Distress, Arms Armor, Chest Armor, Leg Armor` },
+            { encounter: "3rd - 'Macrocosm'", desc: `${icons.strand} Rufus's Fury, ${icons.solar} Acasia's Dejection, ${icons.strand} Mykel's Reverence, ${icons.strand} Koraxis's Distress, Chest Armor, Leg Armor, Class Armor` },
+            { encounter: "4th - 'Nezarec, Final god of Pain'", desc: `Any Weapon, ${icons.stasis} ${icons.exoticengram} Conditional Finality, Head Armor, Leg Armor, Class Armor` },
+        ],
+        notes: "-"
     },
     ce:  {
         name: "Crota's End",
         entries: [
-
-        ]
+            { encounter: "1st - 'The Abyss'", desc: `${icons.arc} Song of Ir Yût, ${icons.solar} Abyss Defiant, ${icons.strand} Fang of Ir Yût, Head Armor, Arms Armor, Chest Armor` },
+            { encounter: "2nd - 'The Bridge'", desc: `${icons.strand} Swordbreaker, ${icons.arc} Oversoul Edict, ${icons.strand} Fang of Ir Yût, Arms Armor, Chest Armor, Leg Armor` },
+            { encounter: "3rd - 'The Deathsinger'", desc: `${icons.arc} Song of Ir Yût, ${icons.arc} Oversoul Edict, ${icons.void} Word of Crota, Chest Armor, Leg Armor, Class Armor` },
+            { encounter: "4th - 'Crota, Son of Oryx'", desc: `${icons.strand} Swordbreaker, ${icons.solar} Abyss Defiant, ${icons.void} Word of Crota, ${icons.kinetic} ${icons.exoticengram} Necrochasm, Head Armor, Leg Armor, Class Armor` },
+        ],
+        notes: "-"
     },
     se:  {
         name: "Salvation's Edge",
         entries: [
-
-        ]
+            { encounter: "1st - 'Substratum'", desc: `${icons.strand} Imminence, ${icons.arc} Non-Denouement, ${icons.solar} Nullify, Arms Armor, Chest Armor` },
+            { encounter: "2nd - 'Herald of Finality'", desc: `${icons.strand} Imminence, ${icons.void} Forthcoming Deviance, ${icons.arc} Non-Denouement, Head Armor, Class Armor` },
+            { encounter: "3rd - 'Repository'", desc: `${icons.stasis} Critical Anomaly, ${icons.void} Forthcoming Deviance, ${icons.solar} Nullify, Arms Armor, Class Armor` },
+            { encounter: "4th - 'Verity'", desc: `${icons.strand} Imminence, ${icons.arc} Summum Bonum, ${icons.arc} Non-Denouement, Chest Armor, Leg Armor` },
+            { encounter: "5th - 'The Witness'", desc: `${icons.stasis} Critical Anomaly, ${icons.arc} Summum Bonum, ${icons.solar} Nullify, ${icons.strand} ${icons.exoticengram} Euphony, Head Armor, Leg Armor` },
+        ],
+        notes: "-"
     },
     d_throne:  {
         name: "The Shattered Throne",
         entries: [
-
-        ]
+            { encounter: "All", desc: "This Dungeon has no specific loot table." }
+        ],
+        notes: "-"
     },
     d_pit:  {
         name: "Pit of Heresy",
         entries: [
-
-        ]
+            { encounter: "All", desc: "This Dungeon has no specific loot table." }
+        ],
+        notes: "-"
     },
     d_proph:  {
         name: "Prophecy",
         entries: [
-
-        ]
+            { encounter: "1st - 'Phalanx Echo'", desc: `${icons.arc} Prosecutor, ${icons.strand} Relentless, Leg Armor, Class Armor` },
+            { encounter: "2nd - 'Hexahedron'", desc: `${icons.kinetic} Adjudicator, ${icons.void} A Sudden Death, Arms Armor` },
+            { encounter: "3rd - 'Kell Echo'", desc: `${icons.stasis} Judgment, ${icons.solar} Darkest Before, Head Armor, Chest Armor, Class Armor + Any 'Moonfang' Armor Piece` },
+        ],
+        notes: "-"
     },
     d_grasp:  {
         name: "Grasp of Avarice",
         entries: [
-
-        ]
+            { encounter: "1st - 'Phry'zia, the Insatiable'", desc: `${icons.arc} Matador 64, Class Armor, Leg Armor` },
+            { encounter: "2nd - 'De-activate the Fallen Shield'", desc: `${icons.arc} Hero of Ages, Arms Armor, Chest Armor` },
+            { encounter: "3rd - 'Avarokk, the Covetous'", desc: `${icons.arc} Matador 64, ${icons.arc} Hero of Ages, ${icons.stasis} Eyasluna, ${icons.void} 1000 Yard Stare, Any Armor` },
+        ],
+        notes: "-"
     },
     d_duality:  {
         name: "Duality",
         entries: [
-
-        ]
+            { encounter: "1st - 'Nightmare of Gahlran'", desc: `${icons.stasis} Lingering Dread, ${icons.void} The Epicurean, Head Armor, Arms Armor, Leg Armor + Any Armor (Master Only)` },
+            { encounter: "2nd - 'Unlock the Vault'", desc: `${icons.arc} Stormchaser, ${icons.void} Unforgiven, Arms Armor, Chest Armor, Class Armor + Any Armor (Master Only)` },
+            { encounter: "3rd - 'Nightmare of Caiatl'", desc: `${icons.stasis} New Purpose, ${icons.solar} Fixed Odds, ${icons.void} ${icons.exoticengram} Heartshadow, Arms Armor, Chest Armor, Class Armor + Any Armor (Master Only)` },
+        ],
+        notes: "-"
     },
     d_spire:  {
         name: "Spire of the Watcher",
         entries: [
-
-        ]
+            { encounter: "1st - 'Ascend the Spire'", desc: `${icons.arc} Long Arm, ${icons.arc} Terminus Horizon, ${icons.kinetic} Seventh Seraph Carbine, Head Armor, Arms Armor, Leg Armor` },
+            { encounter: "2nd - 'Silence the Spire'", desc: `${icons.arc} Terminus Horizon, ${icons.kinetic} Seventh Seraph Officer Revolver, Arms Armor, Chest Armor, Class Armor` },
+            { encounter: "3rd - 'Persys, Primordial Ruin'", desc: `${icons.stasis} Liminal Vigil, ${icons.void} Wilderflight, Any from Previous Encounters, ${icons.solar} ${icons.exoticengram} Hierarchy of Needs, Any Armor` },
+        ],
+        notes: "-"
     },
     d_gotd:  {
         name: "Ghosts of the Deep",
         entries: [
-
-        ]
+            { encounter: "1st - 'Hive Ritual Disrupted'", desc: `${icons.stasis} New Pacific Epitath, ${icons.stasis} Cold Comfort, ${icons.solar} No Survivors, Head Armor, Arms Armor, Leg Armor` },
+            { encounter: "2nd - 'Ecthar, Shield of Savathûn'", desc: `${icons.solar} Greasy Luck, ${icons.stasis} Cold Comfort, ${icons.solar} No Survivors, Arms Armor, Chest Armor, Class Armor` },
+            { encounter: "3rd - 'Šimmumah ur-Nokru'", desc: `Any from Previous Encounters, ${icons.strand} ${icons.exoticengram} The Navigator, Any Armor` },
+        ],
+        notes: "-"
     },
     d_warlords:  {
         name: "Warlord's Ruin",
         entries: [
-
-        ]
+            { encounter: "1st - 'Rathil'", desc: `${icons.arc} Indebted Kindness, ${icons.strand} Vengful Whisper, ${icons.strand} Dragoncult Sickle, Head Armor, Arms Armor, Leg Armor` },
+            { encounter: "2nd - 'Locus of Wailing Grief'", desc: `${icons.arc} Indebted Kindness, ${icons.strand} Vengeful Whisper, ${icons.strand} Naeem's Lance, Arms Armor, Chest Armor, Class Armor` },
+            { encounter: "3rd - 'Hefnd's Vengeance'", desc: `Any from Previous Encounters, ${icons.void} ${icons.exoticengram} Buried Bloodline, Any Armor` },
+        ],
+        notes: "-"
     },
     d_vesper:  {
         name: "Vesper's Host",
         entries: [
-
-        ]
+            { encounter: "1st - 'Activation'", desc: `${icons.stasis} VS Chill Inhibitor, ${icons.void} VS Gravitic Arrest, ${icons.void} VS Velocity Baton, Arms Armor, Chest Armor, Leg Armor` },
+            { encounter: "2nd - 'Raneiks Unified'", desc: `${icons.stasis} VS Chill Inhibitor, ${icons.void} VS Gravitic Arrest, ${icons.arc} VS Pyroelectric Propellant, Head Armor, Arms Armor, Leg Armor` },
+            { encounter: "3rd - 'The Corrupted Puppeteer'", desc: `Any from Previous Encounters, ${icons.solar} ${icons.exoticengram} Icebreaker, Head Armor, Chest Armor, Leg Armor` },
+        ],
+        notes: "The Class item can only be acquired through the 'Rogue Network' quest"
     },
     d_sundered: {
         name: "Sundered Doctrine",
         entries: [
-
-        ]
+            { encounter: "1st - 'Solve the Riddle'", desc: `${icons.strand} Unloved, ${icons.strand} Unsworn, ${icons.arc} Unworthy, Head Armor, Chest Armor, Leg Armor` },
+            { encounter: "2nd - 'Zoetic Lockset'", desc: `${icons.strand} Unloved, ${icons.strand} Unsworn, ${icons.void} Unvoiced, Head Armor, Arms Armor, Leg Armor` },
+            { encounter: "3rd - 'Kerrev, the Erased'", desc: `Any from Previous Encounters, ${icons.solar} ${icons.exoticengram} Finality's Auger, Any Armor` },
+        ],
+        notes: "The Class item can only be acquired through the 'Drowning Labyrinth' quest (or master clears)."
     },
 }
 
@@ -215,7 +276,17 @@ module.exports = {
             try {
                 let embed;
                 if (graphical && reverseImage[category] !== false) {
-
+                    // TODO: Fix this processTime to be in the right place, I'm too lazy right now
+                    const processTime = Date.now() - interaction.createdTimestamp;
+                
+                    embed = new EmbedBuilder()
+                        .setColor(0x00FF00)
+                        .setTitle(reverseDesc[category].name + " Loot Table")
+                        .setAuthor({ name: "Destiny Compendium" })
+                        .setDescription("*Credit to nietcool for creating the graphic.*")
+                        .setTimestamp()
+                        .setImage(reverseImage[category])
+                        .setFooter({ text: `Processed in ${processTime} ms` });
                 } else {
                     // TODO: Fix this processTime to be in the right place, I'm too lazy right now
                     const processTime = Date.now() - interaction.createdTimestamp;
@@ -231,6 +302,12 @@ module.exports = {
                     const data = reverseDesc[category].entries;
                     for (let i = 0; i < data.length; i++) {
                         embed.addFields({ name: data[i].encounter, value: data[i].desc });
+                    }
+
+                    embed.addFields({ name: "Additional Notes", value: reverseDesc[category].notes });
+
+                    if (graphical) {
+                        embed.setDescription("The Graphic for this activity doesn't exist, so you've been pointed to this instead.");
                     }
                 }
 
