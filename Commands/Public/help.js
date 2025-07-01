8e0d5e1058e233baee61d25279f0a10d4e0afc54
DcruBro <jonaskn914@gmail.com>
2025-07-01 18:12:44 +0200

    Final touches for the 1.3.0 update

--- Commands/Public/help.js
+++ Commands/Public/help.js
@@ -57,6 +57,7 @@ module.exports = {
                     .setDescription(
                         `**/query <category> <query>**\nQueries the Destiny Compendium spreadsheet for your desired **query** in the selected **category**.\n
                         **/rotation <category>**\nReturns the current featured activity rotation for the specified category.\n
+                        **/loottable <activity> [graphical]\nReturns the per-encounter loot table for the specified activity (can set 'graphical' to True for a graphical loot table instead).\n
                         **/help**\nDisplay this menu.\n
                         **/info**\nDisplay bot info\n
                         **/patchnotes**\nBot patch notes\n
